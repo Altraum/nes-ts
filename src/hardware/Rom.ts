6d679c77572a1c59a130c68f36c7d3beb6fd75bf
Basil <great.ore@gmail.com>
2025-10-06 15:20:38 -0400

    Added Opcodes and updated flags 6 and 7 in Rom

--- src/hardware/Rom.ts
+++ src/hardware/Rom.ts
@@ -3,9 +3,12 @@ export class Rom {
     rom_signature : string;
     prg_units : number;
     chr_units : number;
-    flag_6 : number;
-    flag_7 : number;
+    flag_6 : string;
+    flag_7 : string;
     mapper : number;
+    has_extra_ram : number;
+    has_trainer : number;
+    prg_data : Uint8Array;
     constructor(rom_raw_array : ArrayBuffer){
         this.rom_data = new Uint8Array(rom_raw_array);
         this.rom_signature = (this.rom_data[0].toString(16) + this.rom_data[1].toString(16)
@@ -15,22 +18,23 @@ export class Rom {
         } else{
             this.prg_units = this.rom_data[4];
             this.chr_units = this.rom_data[5];
-            this.flag_6 = this.rom_data[6];
-            this.flag_7 = this.rom_data[7];
-            const mapper_lower = this.flag_6.toString(2);
-            const mapper_higher = this.flag_7.toString(2);
+            this.flag_6 = this.rom_data[6].toString(2).padStart(8, "0");
+            this.flag_7 = this.rom_data[7].toString(2).padStart(8, "0");
             console.log(
                 "Combined Mapper: "
-                + mapper_lower.padStart(8, "0").slice(0,4)
-                + mapper_higher.padStart(8, "0").slice(0,4)
+                + this.flag_7.slice(0,4)
+                + this.flag_6.slice(0,4)
             );
             this.mapper = parseInt(
-                mapper_lower.padStart(8, "0").slice(0,4)
-                + mapper_higher.padStart(8, "0").slice(0,4)
+                this.flag_7.slice(0,4)
+                + this.flag_6.slice(0,4)
             );
+            this.has_extra_ram = parseInt(this.flag_6.at(7-1));
+            this.has_trainer = parseInt(this.flag_6.at(7-2));
             console.log("Flag 6: " + this.flag_6.toString(2));
             console.log("Flag 7: " + this.flag_7.toString(2));
             console.log("Mapper : " + this.mapper);
+            this.prg_data = this.rom_data.slice(16, 16 + 16384);
         }
     }
 }
