b61d7faed0c55590f81239475e802b0d8a753255
Basil <great.ore@gmail.com>
2025-11-14 11:45:56 -0500

    Made Nes class to manage hardware components and updated FileLoader to use it

---
+++ src/hardware/Nes.ts
@@ -0,0 +1,19 @@
+import {Cpu} from "./Cpu.ts";
+import {Rom} from "./Rom.ts";
+
+let rom : Rom
+let cpu : Cpu
+
+export class Nes {
+    getRom() {
+        return rom;
+    }
+
+    setRom( romBinary : ArrayBuffer){
+        rom = new Rom(romBinary);
+    }
+
+    runCpu(){
+        cpu = new Cpu(rom);
+    }
+}
